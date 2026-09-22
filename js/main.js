a90b9cb82ef42333facb6945a26e3b91038c988a
Aman Kumar <ak9910450375@gmail.com>
2026-09-22 10:15:17 +0530

    Added minor changes and standardized headfor metadata

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const backToTopButton = document.querySelector(".back-to-top");
+
+if (backToTopButton) {
+    backToTopButton.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+}
